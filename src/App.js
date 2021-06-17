47635fba3ebb5390b26e71ce7370c626759ada33
Allard <AllardQuek@users.noreply.github.com>
2021-06-17 23:50:37 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Enabling Emmet</title>
+        <meta name="description" content="Enabling Emmet in VS Code" />
+      </Helmet>
+    </div>
+  );
+}
+
+export default App;
